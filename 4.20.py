3f9cda19f10123b0e2faea97b94626190387e958
mdunghk <78392069+mdunghk@users.noreply.github.com>
2021-05-30 20:29:11 +0700

    Add files via upload

---
+++ 4.20.py
@@ -0,0 +1,20 @@
+import math
+
+def combination(n, r):
+    return int((math.factorial(n)) / ((math.factorial(r)) * math.factorial(n - r)))
+
+def for_test(x, y):
+    for y in range(x):
+        return combination(x, y)
+
+def pascals_triangle(rows):
+    result = []
+    for count in range(rows): 
+        row = []
+        for element in range(count + 1): 
+            row.append(combination(count, element))
+        result.append(row)
+    return result
+
+for row in pascals_triangle(3):
+    print(row)
